b85c25a09f32f54b7f2f852b41f51f688acb3c53
shuqi.qin <shuqi.qin@qunar.com>
2016-03-15 19:35:12 +0800

    new proj

---
+++ src/main/java/LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,10 @@
+/**
+ Given a string, find the length of the longest substring without repeating characters. For example, the longest substring without repeating letters for "abcabcbb" is "abc", which the length is 3. For "bbbbb" the longest substring is "b", with the length of 1.
+ */
+public class LongestSubstringWithoutRepeatingCharacters {
+    public int lengthOfLongestSubstring(String s) {
+
+        return -1;
+
+    }
+}
---
+++ src/test/java/Test.java
@@ -0,0 +1,15 @@
+/**
+ * Created by shuqi.qin on 2016/3/15.
+ */
+public class Test {
+
+    public static void main(String[] args){
+
+        int x = 4&3;
+
+        x = 5&3;
+
+        System.out.println();
+    }
+
+}
